8440877cfa2ec3c49837287a0fe62aa6b7c0ac44
Ari Lima <alima@redhat.com>
2019-06-07 10:58:54 -0400

    improvements to cluster list and cluster describe

--- cmd/uhc/cluster/describe/describe.go
+++ cmd/uhc/cluster/describe/describe.go
@@ -17,34 +17,51 @@ limitations under the License.
 package describe
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/openshift-online/uhc-cli/pkg/config"
 	"github.com/openshift-online/uhc-cli/pkg/util"
 	"github.com/openshift-online/uhc-sdk-go/pkg/client"
+	v1 "github.com/openshift-online/uhc-sdk-go/pkg/client/clustersmgmt/v1"
 
 	"github.com/spf13/cobra"
 )
 
 var args struct {
-	debug bool
+	debug  bool
+	json   bool
+	output bool
 }
 
 var Cmd = &cobra.Command{
-	Use:   "describe CLUSTERID",
+	Use:   "describe CLUSTERID [--output] [--short]",
 	Short: "Decribe a cluster",
 	Long:  "Get info about a cluster identified by its cluster ID",
 	Run:   run,
 }
 
 func init() {
+	// Add flags to rootCmd:
 	flags := Cmd.Flags()
 	flags.BoolVar(
 		&args.debug,
 		"debug",
 		false,
 		"Enable debug mode.",
+	)
+	flags.BoolVar(
+		&args.output,
+		"output",
+		false,
+		"Output result into JSON file.",
+	)
+	flags.BoolVar(
+		&args.json,
+		"json",
+		false,
+		"Output the entire JSON structure",
 	)
 }
 
@@ -108,26 +125,82 @@ func run(cmd *cobra.Command, argv []string) {
 	clusterResource := resource.Cluster(argv[0])
 
 	// Retrieve the collection of clusters:
-	response, err := clusterResource.Get().
-		Send()
+	response, err := clusterResource.Get().Send()
+
+	// Get cluster body:
+	cluster := response.Body()
+
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Can't retrieve clusters: %s", err)
 		os.Exit(1)
 	}
 
-	cluster := response.Body()
-	fmt.Printf("ID:       %s\n"+
-		"Name:     %s.%s\n"+
-		"Masters:  %d\n"+
-		"Computes: %d\n"+
-		"Region:   %s\n"+
-		"Multi-az: %t\n",
-		cluster.ID(),
-		cluster.Name(),
-		cluster.DNS().BaseDomain(),
-		cluster.Nodes().Master(),
-		cluster.Nodes().Compute(),
-		cluster.Region().ID(),
-		cluster.MultiAZ(),
-	)
+	if args.output {
+		// Create a filename based on cluster name:
+		filename := fmt.Sprintf("cluster-%s.json", cluster.ID())
+
+		// Attempt to create file:
+		myFile, err := os.Create(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to create file: %v\n", err)
+			os.Exit(1)
+		}
+
+		// Reasign encoder io.Writer to file writer:
+		encoder := json.NewEncoder(myFile)
+		encoder.SetIndent("", " ")
+
+		// Dump encoder content into file:
+		err = v1.MarshalCluster(cluster, encoder)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to Marshal cluster into file: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	// Get full API response (JSON):
+	if args.json {
+		// Get JSON encoder:
+		fmt.Println()
+		encoder := json.NewEncoder(os.Stdout)
+		encoder.SetIndent("", " ")
+
+		// Convert cluster to JSON and dump to encoder:
+		err = v1.MarshalCluster(cluster, encoder)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to Marshal cluster into JSON encoder: %v\n", err)
+			os.Exit(1)
+		}
+	} else {
+		// Get creation date info:
+		clusterTimetamp := cluster.CreationTimestamp()
+		year, month, day := clusterTimetamp.Date()
+
+		// Get API URL:
+		api := cluster.API()
+		apiURL, _ := api.GetURL()
+
+		// Print short cluster description:
+		fmt.Printf("\nID:       %s\n"+
+			"Name:     %s.%s\n"+
+			"API URL:  %s\n"+
+			"Masters:  %d\n"+
+			"Computes: %d\n"+
+			"Region:   %s\n"+
+			"Multi-az: %t\n"+
+			"Creator:  %s\n"+
+			"Created:  %s %d %d\n",
+			cluster.ID(),
+			cluster.Name(),
+			cluster.DNS().BaseDomain(),
+			apiURL,
+			cluster.Nodes().Master(),
+			cluster.Nodes().Compute(),
+			cluster.Region().ID(),
+			cluster.MultiAZ(),
+			cluster.Creator(),
+			month.String(), day, year,
+		)
+		fmt.Println()
+	}
 }
