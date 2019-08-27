11b699ee0f67df13a2bd52b9fdb6c41c0d636a0b
Juan Hernandez <jhernand@redhat.com>
2019-08-27 15:06:56 +0200

    Rename to `ocm`
    This patch renames the tool to `ocm` and adjusts the code for the new
    home at `github.com/openshift-online/ocm-cli`.
    
    Signed-off-by: Juan Hernandez <jhernand@redhat.com>

---
+++ cmd/ocm/account/orgs/cmd.go
@@ -0,0 +1,175 @@
+/*
+Copyright (c) 2019 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package orgs
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/openshift-online/ocm-cli/pkg/config"
+	flags "github.com/openshift-online/ocm-cli/pkg/flags"
+	table "github.com/openshift-online/ocm-cli/pkg/table"
+	amv1 "github.com/openshift-online/ocm-sdk-go/accountsmgmt/v1"
+)
+
+var args struct {
+	columns   string
+	parameter []string
+	padding   int
+}
+
+// Cmd ...
+var Cmd = &cobra.Command{
+	Use:   "orgs",
+	Short: "List organizations.",
+	Long:  "Display a list of organizations.",
+	RunE:  run,
+}
+
+func init() {
+	// Add flags to rootCmd:
+	fs := Cmd.Flags()
+	flags.AddParameterFlag(fs, &args.parameter)
+	fs.StringVar(
+		&args.columns,
+		"columns",
+		"id,href,name", // Default value gets assigned later as connection is needed.
+		"Organization identifier. Defaults to the organization of the current user.",
+	)
+	fs.IntVar(
+		&args.padding,
+		"padding",
+		45,
+		"Takes padding for custom columns, default to 45.",
+	)
+}
+
+func run(cmd *cobra.Command, argv []string) error {
+
+	// Load the configuration file:
+	cfg, err := config.Load()
+	if err != nil {
+		return fmt.Errorf("Can't load config file: %v", err)
+	}
+	if cfg == nil {
+		return fmt.Errorf("Not logged in, run the 'login' command")
+	}
+
+	// Check that the configuration has credentials or tokens that haven't have expired:
+	armed, err := cfg.Armed()
+	if err != nil {
+		return fmt.Errorf("Can't check if tokens have expired: %v", err)
+	}
+	if !armed {
+		return fmt.Errorf("Tokens have expired, run the 'login' command")
+	}
+
+	// Create the connection, and remember to close it:
+	connection, err := cfg.Connection()
+	if err != nil {
+		return fmt.Errorf("Can't create connection: %v", err)
+	}
+	defer connection.Close()
+
+	// Indices
+	pageIndex := 1
+	pageSize := 100
+
+	// Setting column names and padding size
+	// Update our column name displaying variable:
+	args.columns = strings.Replace(args.columns, " ", "", -1)
+	colUpper := strings.ToUpper(args.columns)
+	colUpper = strings.Replace(colUpper, ".", " ", -1)
+	columnNames := strings.Split(colUpper, ",")
+	paddingByColumn := []int{29, 65, 70}
+	if args.columns != "id,href,name" {
+		paddingByColumn = []int{args.padding}
+	}
+
+	// Print Header Row:
+	table.PrintPadded(os.Stdout, columnNames, paddingByColumn)
+	fmt.Println()
+
+	for {
+		// Next page request:
+		request := connection.AccountsMgmt().V1().Organizations().
+			List().
+			Page(pageIndex).
+			Size(pageSize)
+
+		// Apply parameters
+		flags.ApplyParameterFlag(request, args.parameter)
+
+		// Fetch next page
+		orgList, err := request.Send()
+		if err != nil {
+			return fmt.Errorf("Failed to retrieve organization list: %v", err)
+		}
+
+		// Display organization information
+		orgList.Items().Each(func(org *amv1.Organization) bool {
+			// String to output marshal -
+			// Map used to parse Organization data -
+			// Writer to body variable:
+			var body string
+			var jsonBody map[string]interface{}
+			boddyBuffer := bytes.NewBufferString(body)
+
+			// Write Organization data to body variable:
+			err := amv1.MarshalOrganization(org, boddyBuffer)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to marshal organization into byte buffer: %s\n", err)
+				os.Exit(1)
+			}
+
+			// Get JSON from Organization bytes
+			err = json.Unmarshal(boddyBuffer.Bytes(), &jsonBody)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to turn organization bytes into JSON map: %s\n", err)
+				os.Exit(1)
+			}
+
+			// Loop through wanted columns and populate an organization instance
+			iter := strings.Split(args.columns, ",")
+			thisOrg := []string{}
+			for _, element := range iter {
+				value, status := table.FindMapValue(jsonBody, element)
+				if !status {
+					value = "NONE"
+				}
+				thisOrg = append(thisOrg, value)
+			}
+			table.PrintPadded(os.Stdout, thisOrg, paddingByColumn)
+			return true
+		})
+
+		// Break if we reach last page
+		if orgList.Size() < pageSize {
+			break
+		}
+
+		pageIndex++
+	}
+	fmt.Println()
+
+	return nil
+}
---
+++ cmd/ocm/cluster/describe/describe.go
@@ -0,0 +1,184 @@
+/*
+Copyright (c) 2019 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package describe
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/openshift-online/ocm-cli/pkg/dump"
+	v1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
+	"github.com/spf13/cobra"
+
+	"github.com/openshift-online/ocm-cli/pkg/config"
+)
+
+var args struct {
+	json   bool
+	output bool
+}
+
+var Cmd = &cobra.Command{
+	Use:   "describe CLUSTERID [--output] [--short]",
+	Short: "Describe a cluster",
+	Long:  "Get info about a cluster identified by its cluster ID",
+	RunE:  run,
+}
+
+func init() {
+	// Add flags to rootCmd:
+	flags := Cmd.Flags()
+	flags.BoolVar(
+		&args.output,
+		"output",
+		false,
+		"Output result into JSON file.",
+	)
+	flags.BoolVar(
+		&args.json,
+		"json",
+		false,
+		"Output the entire JSON structure",
+	)
+}
+
+func run(cmd *cobra.Command, argv []string) error {
+
+	if len(argv) != 1 {
+		return fmt.Errorf("Expected exactly one cluster")
+	}
+
+	// Load the configuration file:
+	cfg, err := config.Load()
+	if err != nil {
+		return fmt.Errorf("Can't load config file: %v", err)
+	}
+	if cfg == nil {
+		return fmt.Errorf("Not logged in, run the 'login' command")
+	}
+
+	// Check that the configuration has credentials or tokens that haven't have expired:
+	armed, err := cfg.Armed()
+	if err != nil {
+		return fmt.Errorf("Can't check if tokens have expired: %v", err)
+	}
+	if !armed {
+		return fmt.Errorf("Tokens have expired, run the 'login' command")
+	}
+
+	// Create the connection, and remember to close it:
+	connection, err := cfg.Connection()
+	if err != nil {
+		return fmt.Errorf("Can't create connection: %v", err)
+	}
+	defer connection.Close()
+
+	// Get the client for the resource that manages the collection of clusters:
+	resource := connection.ClustersMgmt().V1().Clusters()
+
+	// Get the resource that manages the cluster that we want to display:
+	clusterResource := resource.Cluster(argv[0])
+
+	// Retrieve the collection of clusters:
+	response, err := clusterResource.Get().Send()
+
+	// Get cluster body:
+	cluster := response.Body()
+
+	if err != nil {
+		return fmt.Errorf("Can't retrieve clusters: %s", err)
+	}
+
+	if args.output {
+		// Create a filename based on cluster name:
+		filename := fmt.Sprintf("cluster-%s.json", cluster.ID())
+
+		// Attempt to create file:
+		myFile, err := os.Create(filename)
+		if err != nil {
+			return fmt.Errorf("Failed to create file: %v", err)
+		}
+
+		// Reasign encoder io.Writer to file writer:
+		encoder := json.NewEncoder(myFile)
+		encoder.SetIndent("", " ")
+
+		// Dump encoder content into file:
+		err = v1.MarshalCluster(cluster, encoder)
+		if err != nil {
+			return fmt.Errorf("Failed to Marshal cluster into file: %v", err)
+		}
+	}
+
+	// Get full API response (JSON):
+	if args.json {
+		// Buffer for pretty output:
+		buf := new(bytes.Buffer)
+		fmt.Println()
+
+		// Convert cluster to JSON and dump to encoder:
+		err = v1.MarshalCluster(cluster, buf)
+		if err != nil {
+			return fmt.Errorf("Failed to Marshal cluster into JSON encoder: %v", err)
+		}
+
+		if response.Status() < 400 {
+			err = dump.Pretty(os.Stdout, buf.Bytes())
+		} else {
+			err = dump.Pretty(os.Stderr, buf.Bytes())
+		}
+		if err != nil {
+			return fmt.Errorf("Can't print body: %v", err)
+		}
+
+	} else {
+		// Get creation date info:
+		clusterTimetamp := cluster.CreationTimestamp()
+		year, month, day := clusterTimetamp.Date()
+
+		// Get API URL:
+		api := cluster.API()
+		apiURL, _ := api.GetURL()
+
+		// Print short cluster description:
+		fmt.Printf("\nID:       %s\n"+
+			"Name:     %s.%s\n"+
+			"API URL:  %s\n"+
+			"Masters:  %d\n"+
+			"Computes: %d\n"+
+			"Region:   %s\n"+
+			"Multi-az: %t\n"+
+			"Creator:  %s\n"+
+			"Created:  %s %d %d\n",
+			cluster.ID(),
+			cluster.Name(),
+			cluster.DNS().BaseDomain(),
+			apiURL,
+			cluster.Nodes().Master(),
+			cluster.Nodes().Compute(),
+			cluster.Region().ID(),
+			cluster.MultiAZ(),
+			cluster.Creator(),
+			month.String(), day, year,
+		)
+		fmt.Println()
+	}
+
+	return nil
+}
