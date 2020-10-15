8110aa1f02a5ef40c2de3ae1ea81f9b3cf47cb11
Beni Cherniavsky-Paskin <cben@redhat.com>
2020-10-16 01:44:27 +0300

    Add `ocm list regions` command
    Hoping to reuse GetRegions() function later for interactive
    `ocm create cluster`, as well as `moactl list regions` and
    `moactl create cluster --interactive`...

--- cmd/ocm/list/cmd.go
+++ cmd/ocm/list/cmd.go
@@ -19,6 +19,7 @@ import (
 	"github.com/openshift-online/ocm-cli/cmd/ocm/list/idp"
 	"github.com/openshift-online/ocm-cli/cmd/ocm/list/ingress"
 	"github.com/openshift-online/ocm-cli/cmd/ocm/list/machinepool"
+	"github.com/openshift-online/ocm-cli/cmd/ocm/list/region"
 	"github.com/openshift-online/ocm-cli/cmd/ocm/list/upgradepolicy"
 	"github.com/openshift-online/ocm-cli/cmd/ocm/list/user"
 	"github.com/openshift-online/ocm-cli/cmd/ocm/list/version"
@@ -37,6 +38,7 @@ func init() {
 	Cmd.AddCommand(idp.Cmd)
 	Cmd.AddCommand(ingress.Cmd)
 	Cmd.AddCommand(machinepool.Cmd)
+	Cmd.AddCommand(region.Cmd)
 	Cmd.AddCommand(upgradepolicy.Cmd)
 	Cmd.AddCommand(user.Cmd)
 	Cmd.AddCommand(version.Cmd)
---
+++ pkg/provider/regions.go
@@ -0,0 +1,57 @@
+/*
+Copyright (c) 2020 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package provider
+
+import (
+	"fmt"
+
+	"github.com/openshift-online/ocm-cli/pkg/cluster"
+	cmv1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
+)
+
+func GetRegions(client *cmv1.Client, provider string, ccs cluster.CCS) (regions []*cmv1.CloudRegion, err error) {
+	if ccs.Enabled {
+		// Build cmv1.AWS object to get list of available regions:
+		awsCredentials, err := cmv1.NewAWS().
+			AccessKeyID(ccs.AWS.AccessKeyID).
+			SecretAccessKey(ccs.AWS.SecretAccessKey).
+			Build()
+		if err != nil {
+			return nil, fmt.Errorf("Failed to build AWS credentials: %v", err)
+		}
+
+		response, err := client.CloudProviders().CloudProvider(provider).AvailableRegions().Search().
+			Page(1).
+			Size(-1).
+			Body(awsCredentials).
+			Send()
+		if err != nil {
+			return nil, err
+		}
+		regions = response.Items().Slice()
+	} else {
+		response, err := client.CloudProviders().CloudProvider(provider).Regions().List().
+			Page(1).
+			Size(-1).
+			Send()
+		if err != nil {
+			return nil, err
+		}
+		regions = response.Items().Slice()
+	}
+	return regions, nil
+}
