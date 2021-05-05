9ffd0dfdab611eb8751c38514f4b004062e9aa89
Gregory Shilin <gshilin@redhat.com>
2021-05-05 16:16:44 +0300

    after review

--- cmd/ocm/pop/job/job.go
+++ cmd/ocm/pop/job/job.go
@@ -14,25 +14,31 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package success
+package job
 
 import (
 	"fmt"
-	"github.com/openshift-online/ocm-cli/pkg/ocm"
+
 	"github.com/spf13/cobra"
+
+	"github.com/openshift-online/ocm-cli/pkg/ocm"
+	"github.com/openshift-online/ocm-sdk-go/jobqueue/v1"
 )
 
 // Cmd Constant:
 var Cmd = &cobra.Command{
-	Use:   "success QUEUE_NAME JOB_ID RECEIPT_ID",
-	Short: "Mark the new Job as a success",
-	Long:  "Mark the new Job as a success on the specified Job Queue.",
-	Args:  cobra.ExactArgs(3),
+	Use:   "job QUEUE_NAME",
+	Short: "Pop (i.e. fetch) a Job",
+	Long:  "Fetch a job from the specified Job Queue.",
+	Args:  cobra.ExactArgs(1),
 	RunE:  run,
 }
 
 func run(_ *cobra.Command, argv []string) error {
-	var err error
+	var (
+		pop *v1.QueuePopResponse
+		err error
+	)
 
 	// Create the client for the OCM API:
 	connection, err := ocm.NewConnection().Build()
@@ -44,11 +50,34 @@ func run(_ *cobra.Command, argv []string) error {
 	// Get the client for the Job Queue management api
 	client := connection.JobQueue().V1()
 
-	// Send a request to Success a Job:
-	_, err = client.Queues().Queue(argv[0]).Jobs().Job(argv[1]).Success().ReceiptId(argv[2]).Send()
+	// Send a request to create the Job:
+	pop, err = client.Queues().Queue(argv[0]).Pop().Send()
 	if err != nil {
-		return fmt.Errorf("unable to success a Job: %v", err)
+		if pop != nil && pop.Status() == 204 {
+			// No Content
+			fmt.Printf("No job found\n")
+			return nil
+		}
+		return fmt.Errorf("unable to fetch a Job: %v", err)
 	}
+	fmt.Printf("{\n"+
+		"  \"id\": \"%s\",\n"+
+		"  \"kind\": \"Job\",\n"+
+		"  \"href\": \"%s\",\n"+
+		"  \"attempts\": \"%d\",\n"+
+		"  \"abandoned_at\": \"%s\",\n"+
+		"  \"created_at\": \"%s\",\n"+
+		"  \"updated_at\": \"%s\",\n"+
+		"  \"receipt_id\": \"%s\",\n"+
+		"}",
+		pop.ID(),
+		pop.HREF(),
+		pop.Attempts(),
+		pop.AbandonedAt(),
+		pop.CreatedAt(),
+		pop.UpdatedAt(),
+		pop.ReceiptId(),
+	)
 
 	return nil
 }
