447488dbe1e0440401f01c55b2f31d499d945769
tithakka <tithakka@redhat.com>
2023-09-20 15:28:25 -0400

    Added default values for CIDRs and host prefix

--- pkg/arguments/interactive.go
+++ pkg/arguments/interactive.go
@@ -295,12 +295,14 @@ func PromptIPNet(fs *pflag.FlagSet, flagName string) error {
 		if flag.Changed {
 			return nil
 		}
-
+		// If the default value is nil, set the value as a default value
+		if flag.DefValue == "<nil>" {
+			flag.DefValue = flag.Value.String()
+		}
 		prompt := &survey.Input{
 			Message: getQuestion(flag),
 			Help:    flag.Usage,
-			// TODO respect flag default, if set
-			// (awkward because https://github.com/golang/go/issues/39516).
+			Default: flag.DefValue,
 		}
 		var response string
 		// Set() flag as side effect of validation => prompts again if invalid.
