9b830b9ac744ba53bd82e1640408b2a6825450e7
tithakka <tithakka@redhat.com>
2024-02-01 10:00:04 -0800

    Add region validation from ocm-shards and list regions command

---
+++ cmd/ocm/list/rhRegion/cmd.go
@@ -0,0 +1,39 @@
+package rhRegion
+
+import (
+	"fmt"
+	sdk "github.com/openshift-online/ocm-sdk-go"
+
+	"github.com/openshift-online/ocm-cli/pkg/config"
+	"github.com/spf13/cobra"
+)
+
+var Cmd = &cobra.Command{
+	Use:     "rh-regions",
+	Short:   "List available OCM regions",
+	Long:    "List available OCM regions",
+	Example: `  # List all supported OCM regions ocm list rh-regions`,
+	RunE:    run,
+}
+
+func run(cmd *cobra.Command, argv []string) error {
+	// Load the configuration file:
+	cfg, err := config.Load()
+	if err != nil {
+		return fmt.Errorf("Can't load config file: %v", err)
+	}
+	if cfg == nil {
+		return fmt.Errorf("Not logged in, run the 'login' command")
+	}
+
+	regions, err := sdk.GetRhRegions(cfg.URL)
+	if err != nil {
+		return fmt.Errorf("Failed to get OCM regions: %w", err)
+	}
+
+	for regionName := range regions {
+		fmt.Println(regionName)
+	}
+	return nil
+
+}
--- cmd/ocm/login/cmd.go
+++ cmd/ocm/login/cmd.go
@@ -59,6 +59,7 @@ var args struct {
 	token         string
 	user          string
 	password      string
+	rhRegion      string
 	insecure      bool
 	persistent    bool
 	useAuthCode   bool
@@ -112,9 +113,16 @@ func init() {
 		&args.url,
 		"url",
 		sdk.DefaultURL,
-		"URL of the API gateway. The value can be the complete URL or an alias. The "+
+		"URL of the APIs gateway. The value can be the complete URL or an alias. The "+
 			"valid aliases are 'production', 'staging', 'integration' and their shorthands.",
 	)
+	flags.StringVar(
+		&args.rhRegion,
+		"rh-region",
+		"",
+		"OCM region identifier. Takes precedence over the --url flag",
+	)
+	//flags.MarkHidden("rh-region")
 	flags.StringVar(
 		&args.token,
 		"token",
@@ -322,6 +330,16 @@ func run(cmd *cobra.Command, argv []string) error {
 		cfg.User = ""
 		cfg.Password = ""
 	}
+
+	// If an OCM region is provided, update the config URL with the SDK generated URL
+	if args.rhRegion != "" {
+		regValue, err := sdk.GetRhRegion(args.url, args.rhRegion)
+		if err != nil {
+			return fmt.Errorf("Can't find region: %w", err)
+		}
+		cfg.URL = fmt.Sprintf("https://%s", regValue.URL)
+	}
+
 	err = config.Save(cfg)
 	if err != nil {
 		return fmt.Errorf("Can't save config file: %v", err)
